db29a0b716c98588a65ae5ec20adfcb5b038d882
Abhishek <abhishekuk2255@gmail.com>
2023-10-19 07:08:55 +0530

    19-10-23 SS 3,4

--- Exp_4/Author_BULK.sql
+++
@@ -1,7 +0,0 @@
-insert into AUTHOR(Author_id, Name, Email, Phone_No, Status) values(1, "Mark Twain", "mark@gmail.com", "908765431", "Writing");
-
-insert into AUTHOR(Author_id, Name, Email, Phone_No, Status) values(2, "Rowling", "rowling@gmail.com", "908765427", "Sleeping");
-
-insert into AUTHOR(Author_id, Name, Email, Phone_No, Status) values(3, "Jane Austen", "jane@gmail.com", "908444434", "Not Writing");
-
-
